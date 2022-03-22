6f0d8f53c197eca317e5eeba4add0de16974da55
Rafael <rcbelima@gmail.com>
2022-03-22 08:04:59 -0300

    "feat: add enum para cirurgias"

---
+++ src/main/kotlin/com/minhacirurgia/minhacirurgiaws/model/enum/SurgeryType.kt
@@ -0,0 +1,14 @@
+package com.minhacirurgia.minhacirurgiaws.model.enum
+
+enum class SurgeryType {
+    SISTEMA_DIGESTORIO,
+    TORACICA,
+    SISTEMA_GENITURINARIO_UROLOGICA,
+    ORTOPEDICA,
+    GINECOLOGICAS_MAMA,
+    NEUROLOGICA,
+    CABECA_PESCOCO,
+    PLASTICA,
+    OFTAMOLOGICA
+
+}
